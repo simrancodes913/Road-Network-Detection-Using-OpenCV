9e9dc2340185b873104f3cf5ae033928e245ec34
Simran <simran.official.in@gmail.com>
2026-05-25 23:12:19 +0530

    Combined all images

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ import matplotlib.pyplot as plt
  you need to convert it to RGB format. However, if you want to display the image as it is without conversion, 
  you can directly use the BGR format with Matplotlib, but it may not display the colors correctly.'''
 image = cv2.imread('data/ROAD.png')
-plt.imshow(image)
-plt.title("Original Image")
-plt.axis('off')
-plt.show()
+#plt.imshow(image)
+#plt.title("Original Image")
+#plt.axis('off')
+#plt.show()
 #BGR to RGB conversion
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-plt.imshow(image_rgb)
-plt.title("Satellite Image")
-plt.axis('off')
-plt.show()
+#plt.imshow(image_rgb)
+#plt.title("Satellite Image")
+#plt.axis('off')
+#plt.show()
 #Converting the image into grayscale
 gray_image=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-plt.imshow(gray_image,cmap='gray')
-plt.title("Grayscale Image")
-plt.axis('off')
-plt.show()
+#plt.imshow(gray_image,cmap='gray')
+#plt.title("Grayscale Image")
+#plt.axis('off')
+#plt.show()
 '''As we will be using the Canny edge detection algorithm, we need to apply
  a Gaussian blur to the image to reduce noise and improve edge detection results.
   Because the Canny edge detection algorithm is sensitive to noise, applying a Gaussian blur
@@ -27,19 +27,56 @@ plt.show()
 '''Without applying Gaussian blur, the Canny edge detection algorithm may produce more
     false edges due to noise in the image.'''
 gaussian_blur = cv2.GaussianBlur(gray_image, (5,5), 0)
-plt.imshow(gaussian_blur, cmap='gray')
-plt.title("Gaussian Blurred Image")
-plt.axis('off')
-plt.show()
+#plt.imshow(gaussian_blur, cmap='gray')
+#plt.title("Gaussian Blurred Image")
+#plt.axis('off')
+#plt.show()
 edges = cv2.Canny(gaussian_blur, 50, 150)
-plt.imshow(edges, cmap='gray')
-plt.title("Canny Edges")
-plt.axis('off')
-plt.show()
+#plt.imshow(edges, cmap='gray')
+#plt.title("Canny Edges")
+#plt.axis('off')
+#plt.show()
 '''Now as  we have done the canny edge detection, we have to do shape processing dilation and erosion to get the better result.'''
 kernel=cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
 dilated_edges=cv2.dilate(edges,kernel,iterations=1)
-plt.imshow(dilated_edges,cmap='gray')
+#plt.imshow(dilated_edges,cmap='gray')
+#plt.title("Dilated Edges")
+#plt.axis('off')
+#plt.show()
+
+'''Plotting all the images at once'''
+plt.figure(figsize=(12,8))
+
+# Original RGB
+plt.subplot(2,3,1)
+plt.imshow(image_rgb)
+plt.title("Original RGB")
+plt.axis('off')
+
+# Grayscale
+plt.subplot(2,3,2)
+plt.imshow(gray_image, cmap='gray')
+plt.title("Grayscale")
+plt.axis('off')
+
+# Gaussian Blur
+plt.subplot(2,3,3)
+plt.imshow(gaussian_blur, cmap='gray')
+plt.title("Gaussian Blur")
+plt.axis('off')
+
+# Canny Edge Detection
+plt.subplot(2,3,4)
+plt.imshow(edges, cmap='gray')
+plt.title("Canny Edge")
+plt.axis('off')
+
+# Dilated Edges
+plt.subplot(2,3,5)
+plt.imshow(dilated_edges, cmap='gray')
 plt.title("Dilated Edges")
 plt.axis('off')
+
+plt.tight_layout()
 plt.show()
+plt.savefig('screenshots/combined_pipeline.png')
